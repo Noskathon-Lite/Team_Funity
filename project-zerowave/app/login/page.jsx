fc293436a13c1b1958979cc9e41790af93b79f2f
Mayank Shahi <crazyorwot0098@gmail.com>
2025-01-12 13:59:51 +0545

    finished login page

--- project-zerowave/app/login/page.jsx
+++ project-zerowave/app/login/page.jsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
 import styles from '../../styles/main2.css';
+import Link from 'next/link';
+export const metadata = {
+  title: 'ZeroWave-Login',
+  icons: {
+    icon: '/img/logo.ico',
+  },
+};
+
 export default function Login() {
   return (
-    <div>
-      <h1>ZeroWavee</h1>
-      
+    <>
+    <div className="flex justify-center items-center " id="login">
+      <img src="/img/zerowave-logo.png" alt="logo" id="usrimg" />
+    
+        <h2 className="text-xl font-[profeshhh] p-[43px]" id="title">zerowave-user</h2>
+        <input type="password" placeholder="password (optional)" id="psd" autoComplete="new-password"/>
+        <a href="/desktop">
+          <img src="/img/arrow.svg" alt="" id="enter"/>
+        </a>
     </div>
+    <div>
+      <img src="/img/pp.png" alt="usr-img" id="ii"/>
+    </div>
+    </>
   );
 }
